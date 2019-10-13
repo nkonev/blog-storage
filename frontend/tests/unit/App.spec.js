e663f8bcbd2ca45abc1295148f5c690a9c5b7120
Nikita Konev <nikit.cpp@yandex.ru>
2019-10-14 02:51:55 +0300

    dummy mock jest

--- frontend/tests/unit/App.spec.js
+++ frontend/tests/unit/App.spec.js
@@ -1,6 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
+jest.mock('@/notifications', () => () => ({
+  error(m, b, s) {
+    console.error("error: "+ m + ", " + b +", "+ s)
+  },
+  simpleError(title, message) {
+    console.error("error: title="+ title+ ", message=" + message)
+  },
+  info(title, message){
+    console.info("info: title="+ title+ ", message=" + message)
+  }
+}));
+
 describe('App.vue', () => {
   it('true', () => {
     expect("true").toMatch("true")
